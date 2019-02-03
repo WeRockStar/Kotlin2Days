b90723727b237b49643bf6d431d8b0f63ff34b05
WeRockStar <kotchaphan.m@gmail.com>
2019-02-03 16:21:41 +0700

    Add more TODO function

---
+++ src/main/kotlin/function/BonusFunction.kt
@@ -0,0 +1,9 @@
+@file:JvmName("StringUtils")
+
+package function
+
+fun main() {
+    //TODO: Named arguments
+    //TODO: Default parameter value (Java overloaded method), Try to work with JavaOverloading
+    //TODO: Static top-level function example with @file:JvmName("StringUtils")
+}
--- src/main/kotlin/java_class/PersonJava.java
+++ src/main/kotlin/java_class/PersonJava.java
@@ -1,3 +1,4 @@
 package java_class;
 
 //TODO: Person class (name, isMarried)
+
